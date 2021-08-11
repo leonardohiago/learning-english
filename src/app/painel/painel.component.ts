42c31eb224d596d9ef9250e3def3b823d8b58f5d
Leonardo Hiago <hiago_leonardo@live.com>
2021-08-11 00:31:13 -0300

    Insertion of logic to update the question and verify the answer

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES;
+  public instrucao = 'Traduza a frase:';
+  public resposta = '';
+
+  public rodada = 0;
+  public rodadaFrase: Frase;
+
+  public progresso = 0;
+
+  constructor() {
+    this.atualizaRodada();
+  }
 
   ngOnInit(): void {
+  }
+
+  atualizaResposta(resposta: string): void {
+    this.resposta = resposta;
+  }
+
+  atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = '';
+  }
+
+  verificaResposta(): void {
+    if (this.resposta.toUpperCase === this.rodadaFrase.frasePtBr.toUpperCase) {
+      alert('Parabéns! A tradução está correta. :)');
+      this.rodada++;
+      this.progresso += 100 / this.frases.length;
+      this.atualizaRodada();
+    } else {
+      alert('Tente novamente! A tradução está incorreta. :(');
+    }
   }
 
 }
