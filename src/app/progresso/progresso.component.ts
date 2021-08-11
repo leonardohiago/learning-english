4390875984df6c67d1546c77e9bb90765dacae3b
Leonardo Hiago <hiago_leonardo@live.com>
2021-08-11 00:30:16 -0300

    Inserting rule to update progress bar

--- src/app/progresso/progresso.component.ts
+++ src/app/progresso/progresso.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progresso',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressoComponent implements OnInit {
 
-  constructor() { }
+  @Input() public progresso: number;
+
+  constructor() {
+    console.log('progresso da tela de progresso:' + this.progresso);
+  }
 
   ngOnInit(): void {
   }
